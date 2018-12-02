e78f8abb43830af5f18d1e93276b0057b220fedf
Hristo-R <hristo.razpopov@abv.bg>
2018-12-03 00:54:30 +0200

    Added Properties in Models

--- SIS-MvcFramework/src/Apps/ChushkaWebApp/Models/Product.cs
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/Models/Product.cs
@@ -1,6 +1,25 @@
-﻿namespace ChushkaWebApp.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ChushkaWebApp.Models
 {
-    class Product
+    public class Product
     {
+        public Product()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductType Type { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
