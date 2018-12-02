4ac4b86cb08090e40d748f4815539ea9be132d6a
Hristo-R <hristo.razpopov@abv.bg>
2018-12-03 01:24:16 +0200

    In Data created class ApplicationDbContext

---
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ChushkaWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChushkaWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        public DbSet<Product> Products { get; set; }
+
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ChushkaDb;Integrated Security=True");
+        }
+    }
+}
