d4825c586b0845ddd925b3dae3d6fe302ede70c4
Hristo-R <hristo.razpopov@abv.bg>
2018-12-10 22:02:16 +0200

    Added IndexViewModel.cs and ProductViewModel.cs

--- SIS-MvcFramework/src/Apps/ChushkaWebApp/Controllers/HomeController.cs
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using SIS.HTTP.Responses;
+﻿using ChushkaWebApp.ViewModels.Home;
+using SIS.HTTP.Responses;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChushkaWebApp.Controllers
@@ -9,6 +11,23 @@ namespace ChushkaWebApp.Controllers
     {
         public IHttpResponse Index()
         {
+            if (this.User.IsLoggedIn)
+            {
+                var products = this.Db.Products.Select(
+                    p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        Description = p.Description
+                    });
+                var model = new IndexViewModel
+                {
+                    Products = products
+                };
+                return this.View("Home/IndexLoggedIn", model);
+            }
+
             return this.View();
         }
     }
---
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/ViewModels/Home/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChushkaWebApp.ViewModels.Home
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
