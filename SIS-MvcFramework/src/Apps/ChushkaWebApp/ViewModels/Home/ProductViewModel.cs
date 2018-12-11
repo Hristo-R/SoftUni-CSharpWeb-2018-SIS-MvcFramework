4ff34302b4fad4b9b0f77a4579c051063b0f776f
Hristo-R <hristo.razpopov@abv.bg>
2018-12-11 22:24:10 +0200

    Added public string ShortDescription in ProductViewModel.cs. Home page ready

--- SIS-MvcFramework/src/Apps/ChushkaWebApp/ViewModels/Home/ProductViewModel.cs
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/ViewModels/Home/ProductViewModel.cs
@@ -9,5 +9,20 @@
         public decimal Price { get; set; }
 
         public string Description { get; set; }
+
+        public string ShortDescription
+        {
+            get
+            {
+                if (this.Description?.Length > 50)
+                {
+                    return this.Description.Substring(0, 50) + " ...";
+                }
+                else
+                {
+                    return this.Description;
+                }
+            }
+        }
     }
 }
