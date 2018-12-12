5148bd887f5696e995bee07b62fa4cead1b8f154
Hristo-R <hristo.razpopov@abv.bg>
2018-12-12 23:43:27 +0200

    Added Product Details Template

---
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ChushkaWebApp.ViewModels.Products;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+using System.Linq;
+
+namespace ChushkaWebApp.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        [Authorize]
+        public IHttpResponse Details(int id)
+        {
+            var viewModel = this.Db.Products
+                .Select(p => new ProductDetailsViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Type = p.Type,
+                    Price = p.Price,
+                })
+                .FirstOrDefault(p => p.Id == id);
+
+            if (viewModel == null)
+            {
+                return this.BadRequestError("Invalid product id.");
+            }
+
+            return this.View(viewModel);
+        }
+    }
+}
