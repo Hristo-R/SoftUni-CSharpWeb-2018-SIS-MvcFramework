9d3e5405d6459101a63e4948ad7dbb8b9bfe519b
Hristo-R <hristo.razpopov@abv.bg>
2019-01-14 16:54:24 +0200

    test WL

---
+++ SIS-MvcFramework/src/Apps/ChushkaWebApp/Controllers/HolidaysController.cs
@@ -0,0 +1,12 @@
+﻿namespace ChushkaWebApp.Controllers
+{
+    using SIS.HTTP.Responses;
+
+    public class HolidaysController : BaseController
+    {
+        public IHttpResponse Sithonia()
+        {
+            return this.View();
+        }
+    }
+}
